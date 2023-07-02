640b71d56671837167306836a8fe895354fdd923
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-07-02 20:24:17 +0530

    [Moulidharan] Added : Delete a person contact in Address Book

--- address_book.py
+++ address_book.py
@@ -16,12 +16,14 @@ class AddressBook:
         self.contacts_dict = {}
 
     def add_contacts(self, contacts_obj):
-        self.contacts_dict.update({contacts_obj.first_name + "," + contacts_obj.last_name: contacts_obj})
+        self.contacts_dict.update({contacts_obj.first_name: contacts_obj})
 
     def update_contacts(self, contacts_obj):
-        self.contacts_dict = {
-            contacts_obj.first_name + "," + contacts_obj.last_name: contacts_obj if contacts_obj.first_name and contacts_obj.last_name in self.contacts_dict else print(
-                "No Contacts Found...")}
+        self.contacts_dict.update({contacts_obj.first_name: contacts_obj}) and print("Contact Updated...") if contacts_obj.first_name and contacts_obj.last_name in self.contacts_dict else print(
+                "No Contacts Found...")
+
+    def delete_contacts(self, first_name):
+        self.contacts_dict.pop(first_name) and print("Contact Deleted...") if first_name in self.contacts_dict else print("No Contacts Found...")
 
     def display(self):
         return self.contacts_dict
@@ -48,6 +50,11 @@ def _update():
     address_book.update_contacts(contacts)
 
 
+def _delete():
+    first_name = input("Enter the First Name : ")
+    address_book.delete_contacts(first_name)
+
+
 def _display():
     print(address_book.display())
 
@@ -56,10 +63,10 @@ if __name__ == '__main__':
     try:
         address_book = AddressBook()
         while True:
-            choice = int(input("\n1.Add Contacts\n2.update Contacts\n3.Display Contacts\n0.Exit\nEnter the Option : "))
+            choice = int(input("\n1.Add Contacts\n2.Update Contacts\n3.Delete Contacts\n4.Display Contacts\n0.Exit\nEnter the Option : "))
             logging.basicConfig(level=logging.INFO, filename="AddressBook_Logs.log", filemode="w",
                                 format="%(asctime)s - %(levelname)s - %(message)s")
-            choice_dict = {1: _add, 2: _update, 3: _display}
+            choice_dict = {1: _add, 2: _update, 3: _delete, 4: _display}
             if choice == 0:
                 print("Exited...")
                 break
