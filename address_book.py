905faee66d577972154eefe40cede8e6ef4e71b5
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-07-02 19:59:02 +0530

    [Moulidharan] Added : Edit existing contacts in Address Book

--- address_book.py
+++ address_book.py
@@ -16,33 +16,55 @@ class AddressBook:
         self.contacts_dict = {}
 
     def add_contacts(self, contacts_obj):
-        self.contacts_dict.update({contacts_obj.first_name + "," + contacts_obj.last_name: contacts})
+        self.contacts_dict.update({contacts_obj.first_name + "," + contacts_obj.last_name: contacts_obj})
+
+    def update_contacts(self, contacts_obj):
+        self.contacts_dict = {
+            contacts_obj.first_name + "," + contacts_obj.last_name: contacts_obj if contacts_obj.first_name and contacts_obj.last_name in self.contacts_dict else print(
+                "No Contacts Found...")}
 
     def display(self):
         return self.contacts_dict
+
+
+def _add_details():
+    first_name = input("Enter the First Name : ")
+    last_name = input("Enter the Last name : ")
+    city = input("Enter the City : ")
+    state = input("Enter the State : ")
+    zip_code = int(input("Enter the Zip Code : "))
+    phone_number = int(input("Enter the Phone Number : "))
+    contacts = Contacts(first_name, last_name, city, state, zip_code, phone_number)
+    return contacts
+
+
+def _add():
+    contacts = _add_details()
+    address_book.add_contacts(contacts)
+
+
+def _update():
+    contacts = _add_details()
+    address_book.update_contacts(contacts)
+
+
+def _display():
+    print(address_book.display())
 
 
 if __name__ == '__main__':
     try:
         address_book = AddressBook()
         while True:
-            choice = int(input("\n1.Add Contacts\n2.Display Contacts\n0.Exit\nEnter the Option : "))
+            choice = int(input("\n1.Add Contacts\n2.update Contacts\n3.Display Contacts\n0.Exit\nEnter the Option : "))
             logging.basicConfig(level=logging.INFO, filename="AddressBook_Logs.log", filemode="w",
                                 format="%(asctime)s - %(levelname)s - %(message)s")
-            if choice == 1:
-                first_name = input("Enter the First Name : ")
-                last_name = input("Enter the Last name : ")
-                city = input("Enter the City : ")
-                state = input("Enter the State : ")
-                zip_code = int(input("Enter the Zip Code : "))
-                phone_number = int(input("Enter the Phone Number : "))
-                contacts = Contacts(first_name, last_name, city, state, zip_code, phone_number)
-                address_book.add_contacts(contacts)
-            elif choice == 2:
-                print(address_book.display())
-            elif choice == 0:
+            choice_dict = {1: _add, 2: _update, 3: _display}
+            if choice == 0:
                 print("Exited...")
                 break
+            else:
+                choice_dict.get(choice)()
             logging.info(f"Address Book is Created...")
     except Exception as ex:
         print(ex)
