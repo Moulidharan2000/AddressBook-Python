8a7703491715e6b46f85393079c4c4fb2bb2c8d2
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-07-02 19:21:56 +0530

    [Moulidharan] Added : Add a new contacts in Address Book

--- address_book.py
+++ address_book.py
@@ -24,18 +24,25 @@ class AddressBook:
 
 if __name__ == '__main__':
     try:
-        logging.basicConfig(level=logging.INFO, filename="AddressBook_Logs.log", filemode="w",
-                            format="%(asctime)s - %(levelname)s - %(message)s")
-        first_name = input("Enter the First Name : ")
-        last_name = input("Enter the Last name : ")
-        city = input("Enter the City : ")
-        state = input("Enter the State : ")
-        zip_code = int(input("Enter the Zip Code : "))
-        phone_number = int(input("Enter the Phone Number : "))
-        contacts = Contacts(first_name, last_name, city, state, zip_code, phone_number)
         address_book = AddressBook()
-        address_book.add_contacts(contacts)
-        print(address_book.display())
-        logging.info(f"Address Book is Created...")
+        while True:
+            choice = int(input("\n1.Add Contacts\n2.Display Contacts\n0.Exit\nEnter the Option : "))
+            logging.basicConfig(level=logging.INFO, filename="AddressBook_Logs.log", filemode="w",
+                                format="%(asctime)s - %(levelname)s - %(message)s")
+            if choice == 1:
+                first_name = input("Enter the First Name : ")
+                last_name = input("Enter the Last name : ")
+                city = input("Enter the City : ")
+                state = input("Enter the State : ")
+                zip_code = int(input("Enter the Zip Code : "))
+                phone_number = int(input("Enter the Phone Number : "))
+                contacts = Contacts(first_name, last_name, city, state, zip_code, phone_number)
+                address_book.add_contacts(contacts)
+            elif choice == 2:
+                print(address_book.display())
+            elif choice == 0:
+                print("Exited...")
+                break
+            logging.info(f"Address Book is Created...")
     except Exception as ex:
         print(ex)
