773cfc65af9337799a4d0905be0ad46f549133e6
Moulidharan2000 <smoulidharanmech@gmail.com>
2023-07-03 11:21:38 +0530

    [Moulidharan] Added : Add multiple person contacts to Address Book

--- address_book.py
+++ address_book.py
@@ -1,5 +1,8 @@
 import logging
 
+logging.basicConfig(level=logging.INFO, filename="AddressBook_Logs.log", filemode="w",
+                    format="%(asctime)s - %(levelname)s - %(message)s")
+
 
 class Contacts:
     def __init__(self, first_name, last_name, city, state, zip_code, phone_number):
@@ -19,14 +22,17 @@ class AddressBook:
         self.contacts_dict.update({contacts_obj.first_name: contacts_obj})
 
     def update_contacts(self, contacts_obj):
-        self.contacts_dict.update({contacts_obj.first_name: contacts_obj}) and print("Contact Updated...") if contacts_obj.first_name and contacts_obj.last_name in self.contacts_dict else print(
-                "No Contacts Found...")
+        self.contacts_dict.update({contacts_obj.first_name: contacts_obj}) and print(
+            "Contact Updated...") if contacts_obj.first_name in self.contacts_dict else print(
+            "No Contacts Found...")
 
     def delete_contacts(self, first_name):
-        self.contacts_dict.pop(first_name) and print("Contact Deleted...") if first_name in self.contacts_dict else print("No Contacts Found...")
+        self.contacts_dict.pop(first_name) and print(
+            "Contact Deleted...") if first_name in self.contacts_dict else print("No Contacts Found...")
 
     def display(self):
-        return self.contacts_dict
+        for i, j in self.contacts_dict.items():
+            print(i, " : ", j)
 
 
 def _add_details():
@@ -56,16 +62,15 @@ def _delete():
 
 
 def _display():
-    print(address_book.display())
+    address_book.display()
 
 
 if __name__ == '__main__':
     try:
         address_book = AddressBook()
         while True:
-            choice = int(input("\n1.Add Contacts\n2.Update Contacts\n3.Delete Contacts\n4.Display Contacts\n0.Exit\nEnter the Option : "))
-            logging.basicConfig(level=logging.INFO, filename="AddressBook_Logs.log", filemode="w",
-                                format="%(asctime)s - %(levelname)s - %(message)s")
+            choice = int(input(
+                "\n1.Add Contacts\n2.Update Contacts\n3.Delete Contacts\n4.Display Contacts\n0.Exit\nEnter the Option : "))
             choice_dict = {1: _add, 2: _update, 3: _delete, 4: _display}
             if choice == 0:
                 print("Exited...")
@@ -75,3 +80,4 @@ if __name__ == '__main__':
             logging.info(f"Address Book is Created...")
     except Exception as ex:
         print(ex)
+        logging.exception(ex)
